9c7b6b09bea568fbea8b4c68980c46d9d01ddf2c
Lucy Fox <lucyreneefox@gmail.com>
2016-07-27 13:31:04 -0600

    Uncommented root page test

--- spec/features/root_page_spec.rb
+++ spec/features/root_page_spec.rb
@@ -1,20 +1,19 @@
-# require 'rails_helper'
-#
-# RSpec.feature "Root Page" do
-#   context "user visits root page" do
-#     scenario "sees all appropriate information" do
-#       pending
-#       VCR.use_cassette("root_page") do
-#         visit "/"
-#
-#         expect(page).to have_content("BeerExplorer")
-#         expect(page).to have_content("Start Exploring")
-#         expect(page).to have_content("Sign in with Google")
-#
-#         expect(page).to have_content("Beer of the Moment")
-#         expect(page).to have_content("ABV")
-#         expect(page).to have_content("IBU")
-#       end
-#     end
-#   end
-# end
+require 'rails_helper'
+
+RSpec.feature "Root Page" do
+  context "user visits root page" do
+    scenario "sees all appropriate information" do
+      VCR.use_cassette("root_page") do
+        visit "/"
+
+        expect(page).to have_content("BeerExplorer")
+        expect(page).to have_content("Start Exploring")
+        expect(page).to have_content("Sign in with Google")
+
+        expect(page).to have_content("Beer of the Moment")
+        expect(page).to have_content("ABV")
+        expect(page).to have_content("IBU")
+      end
+    end
+  end
+end
