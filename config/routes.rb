5a4b06da2a7e7cc7c1c7b34e6e19d619cf6a612f
Lucy Fox <lucyreneefox@gmail.com>
2016-07-30 12:33:25 -0600

    User has dashboard when logged in can view dashboard for current user

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'auth/google_oauth2/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
+  get '/dashboard', to: 'users#show'
 
   resources :styles, only: [:index, :show]
   resources :sessions, only: [:create, :destroy]
---
+++ spec/features/logged_in_user_views_dashboard_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.feature "Logged in User" do
+  scenario "views their dashboard" do
+    user = User.create(name: "Lucy Fox")
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return( user )
+
+    visit dashboard_path
+
+    expect(page).to have_content("Signed in as Lucy Fox")
+    expect(page).to have_content("Welcome, Lucy Fox")
+    expect(page).to have_content("Sorry, you do not have any rated beers. Go rate some beers!")
+    expect(page).to have_content("Please rate beers in order to get recommendations")
+
+  end
+end
