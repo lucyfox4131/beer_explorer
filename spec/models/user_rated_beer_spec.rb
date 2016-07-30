2ff00c50858b8b17850a0fb9fabcbf519a4d71dc
Lucy Fox <lucyreneefox@gmail.com>
2016-07-30 15:39:51 -0600

    All relationships established, testing, and passing all tests between rated beers, breweries, and user

--- spec/models/user_rated_beer_spec.rb
+++ spec/models/user_rated_beer_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe UserRatedBeer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "relationships" do
+    it { should belong_to(:user) }
+    it { should belong_to(:rated_beer) }
+  end
 end
