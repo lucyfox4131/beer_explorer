99326479bc2246e3abbaddb04e5921d9a479f796
Lucy Fox <lucyreneefox@gmail.com>
2016-08-02 14:24:16 -0600

    cleaned up react search divs

--- app/assets/javascripts/components/_beer_name_search.js.jsx
+++ app/assets/javascripts/components/_beer_name_search.js.jsx
@@ -2,15 +2,13 @@ var BeerNameSearch = React.createClass({
   render() {
     return (
     <div class="row">
-      <div class="col-sm-2"></div>
       <div class="col-sm-8">
-        <div class="form-group name-search">
+        <div class="form-group name-search text-center">
           <input class="form-control" type="text" id="beer-name"></input>
+          <input class="btn btn-default pull-right" id="search-by-name" type="button" name="submit" value="Search By Name"></input>
         </div>
-        <input class="btn btn-default pull-right" id="search-by-name" type="button" name="submit" value="Search By Name"></input>
         <hr></hr>
       </div>
-      <div class="col-sm-2"></div>
     </div>
     )
   }
