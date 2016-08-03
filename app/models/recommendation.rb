602ea8e86a8958e56e7996d92de2918583a3e31b
Lucy Fox <lucyreneefox@gmail.com>
2016-08-03 15:00:39 -0600

    Beginning tests for recommendations addition

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -1,4 +1,8 @@
 class Recommendation < ApplicationRecord
   has_many :user_recommendations
   has_many :users, through: :user_recommendations
+
+  def self.generate_recs(rating, beer_id, current_user)
+    
+  end
 end
--- spec/models/recommendation_spec.rb
+++ spec/models/recommendation_spec.rb
@@ -5,4 +5,20 @@ RSpec.describe Recommendation, type: :model do
     it { should have_many(:user_recommendations) }
     it { should have_many(:users) }
   end
+
+  context "generating new user recommendations" do
+    it "wont generate a rating if beer is disliked" do
+      rating = 0
+      user = create(:user, name: "Lucy")
+      beer_id = "XXgGZ4"
+
+      expect(Recommendation.count).to eq(0)
+      expect(UserRecommendation.count).to eq(0)
+
+      Recommendation.generate_recs(rating, beer_id, user)
+
+      expect(Recommendation.count).to eq(0)
+      expect(UserRecommendation.count).to eq(0)
+    end
+  end
 end
