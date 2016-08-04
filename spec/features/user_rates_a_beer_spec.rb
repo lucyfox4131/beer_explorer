454b3a7dfaf72fa2a17c814f2ec2432354d3d6fa
Lucy Fox <lucyreneefox@gmail.com>
2016-08-03 23:26:49 -0600

    Added all ratings nav link and test to cover sad path for rating a beer when not logged in

--- spec/features/user_rates_a_beer_spec.rb
+++ spec/features/user_rates_a_beer_spec.rb
@@ -21,4 +21,19 @@ RSpec.feature "User rates a beer" do
     expect(page).to have_content("You've rated Cutthroat Porter")
     expect(current_path).to eq(beer_path("XXgGZ4"))
   end
+
+  scenario "Visitor tries to rate beer" do
+    visit beer_path("XXgGZ4")
+
+    expect(page).to have_content("Cutthroat Porter")
+    expect(page).to have_content("Rate Cutthroat Porter")
+
+    within(".rate-beer") do
+      expect(page).to have_button("Like")
+      expect(page).to have_button("Dislike")
+      click_button "Like"
+    end
+
+    expect(page).to have_content("Please login to rate this beer")
+  end
 end
